858c0476e3fcabe3469109c17566ab5386281f03
FriederikeBiermann <friederike@biermann-erfurt.de>
2023-06-23 17:25:55 +0200

    Removed bugs in CRF

--- Conditional random field classifier/crf_classifier.py
+++ Conditional random field classifier/crf_classifier.py
@@ -8,24 +8,23 @@ from sklearn.model_selection import train_test_split
 from plotting_results import *
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score, f1_score
 import joblib
-
-# Example usage:
-directory = '/path/to/your/directory'
-output_directory = ""
+import argparse
+from collections import defaultdict
 
 
-def load_data_from_directory(directory):
+def load_data_from_file(file_path):
     data = []
-    for filename in os.listdir(directory):
-        if filename.endswith(".txt"):  # Only process .txt files
-            with open(os.path.join(directory, filename), 'r') as file:
-                for line in file:
-                    # Use ast.literal_eval to convert string to dictionary
-                    sequence = ast.literal_eval(line.strip())
-                    data.append(sequence)
+    if file_path.endswith(".txt"):  # Only process .txt files
+        with open(file_path, 'r') as file:
+            for line in file:
+                # Use ast.literal_eval to convert string to dictionary
+                sequence = ast.literal_eval(line.strip())
+                data.append(sequence)
+    print("loaded data")
     return data
 
-def split_data(data, test_size=0.2, random_state=42):
+
+def split_data(data, test_size=0.5, random_state=42):
     """
     Split the data into training and test sets.
 
@@ -72,20 +71,21 @@ def extract_features_and_labels(data):
                     mean, std = feature_mean_std[key]
                     normalized_value = (value - mean) / std if std > 0 else 0.0
                     feature_vector.append(f'{key}={normalized_value}') 
-            labels_sequence.append (dictionary['gene'])
+            labels_sequence.append ("gene" if int(dictionary['gene'])==1 else "no-gene")
             features_sequence.append(feature_vector)
         labels.append(labels_sequence)
         features.append (features_sequence)
     return features, labels
 
 def evaluate_on_validation_set(y_true, y_pred):
-    y_pred_binary = [1 if y_pred_instance >= 0.5 else 0 for y_pred_instance in y_pred]
+    y_pred_binary = ["gene" if y_pred_instance >= 0.5 else "no-gene" for y_pred_instance in y_pred]
     # Calculate the metrics
     accuracy = accuracy_score(y_true, y_pred_binary)
     balanced_accuracy = balanced_accuracy_score(y_true, y_pred_binary)
-    precision = precision_score(y_true, y_pred_binary)
-    recall = recall_score(y_true, y_pred_binary)
-    f1 = f1_score(y_true, y_pred_binary)
+    precision = precision = precision_score(
+        y_true, y_pred_binary, pos_label='gene')
+    recall = recall_score(y_true, y_pred_binary, pos_label='gene')
+    f1 = f1_score(y_true, y_pred_binary, pos_label='gene')
 
     metrics = {
         'accuracy': accuracy,
@@ -94,15 +94,15 @@ def evaluate_on_validation_set(y_true, y_pred):
         'recall': recall,
         'f1_score': f1
     }
-
     return metrics
 
-def train_crf_classifier(features_list, labels_list, hyperparameters, labels_test_list, features_test_list):
+def train_crf_classifier(features_list, labels_list, hyperparameters, labels_test_list, features_test_list, file):
     trainer = pycrfsuite.Trainer(verbose=False)
 
     # Loop over each sequence of features and labels and append them
     for features, labels in zip(features_list, labels_list):
         trainer.append(features, labels)
+    print("loaded_data")
 
     c1, c2 = hyperparameters  # Unpack hyperparameters
     trainer.set_params({
@@ -112,33 +112,31 @@ def train_crf_classifier(features_list, labels_list, hyperparameters, labels_tes
         'feature.possible_transitions': True  # include possible transitions
     })
     
-    model_filename = f'crf_model_c1_{c1}_c2_{c2}.crfsuite'
-    trainer.train(model_filename)  # Use different file name for each model
-    
+    filename_base = os.path.split(file)[1]
+    model_filename = f'crf_model_{filename_base}_c1_{c1}_c2_{c2}.crfsuite'
     path_filename = os.path.join(output_directory, model_filename)
     trainer.train(path_filename)  # Use different file name for each model
-    
-    # Saving the model using joblib
-    joblib.dump(trainer, model_filename)
+    print("trained_model")
+
 
     # Predict and evaluate for each test sequence separately, then combine results
-    performances = []
+    all_y_pred = []
+    all_y_labels = []
     for features_test, labels_test in zip(features_test_list, labels_test_list):
-        y_pred = predict_gene_probability(features_test, model_filename)
-        performance = evaluate_on_validation_set(labels_test, y_pred)  # Replace with your own function to evaluate the model
-        performances.append(performance)
+            y_pred = predict_gene_probability(features_test, path_filename)
+            all_y_pred.extend(y_pred)
+            all_y_labels.extend(labels_test)
+    performance = evaluate_on_validation_set(all_y_labels, all_y_pred)
 
-    # Here you could return an average performance, or the performances list itself, depending on your needs
-    avg_performances = {key: sum([performance[key] for performance in performances])/len(performances) for key in performances[0]}
-    return avg_performances
-
+    return performance
 
 
-def hyperparameter_search(features, labels, hyperparameters, labels_test, features_test):
+
+def hyperparameter_search(features, labels, hyperparameters, labels_test, features_test, file):
     # Create a pool of worker processes
     with multiprocessing.Pool() as pool:
         # Use starmap to apply train_crf_classifier to each set of hyperparameters in parallel
-        results = pool.starmap(train_crf_classifier, [(features, labels, hp, labels_test, features_test) for hp in hyperparameters])
+        results = pool.starmap(train_crf_classifier, [(features, labels, hp, labels_test, features_test, file) for hp in hyperparameters])
     # Determine the best hyperparameters by comparing the performances
     f1_scores = [result['f1_score'] for result in results]
     best_index = f1_scores.index(max(f1_scores))
@@ -147,54 +145,83 @@ def hyperparameter_search(features, labels, hyperparameters, labels_test, featur
     
     return best_hyperparameters, best_performance
 
+
 def predict_gene_probability(features_test, name_classifier_with_best_performance):
     gene_probabilities = []
     for sequence in features_test:
         tagger = pycrfsuite.Tagger()
         tagger.open(name_classifier_with_best_performance)
 
-        sequence_label_probabilities = tagger.marginal(sequence)
-        gene_probabilities.append(sequence_label_probabilities[:, tagger.labelid('gene')].mean(axis=1)) 
-    print (gene_probabilities)
+        sequence_label_probabilities = [tagger.marginal(
+            'gene', t) for t in range(len(sequence))]
+        gene_probabilities.append(np.mean(sequence_label_probabilities))
     return gene_probabilities
 
-# Generate a list of hyperparameters to try
-c1_values = np.logspace(-3, 3, 7)  # Values for c1
-c2_values = np.logspace(-3, 3, 7)  # Values for c2
-hyperparameters = list(product(c1_values, c2_values))  # All combinations of c1 and c2
+def main(file, output_directory):
+    filename_base = os.path.split(file)[1]
+    # Generate a list of hyperparameters to try
+    c1_values = np.logspace(-3, 3, 3)  # Values for c1
+    c2_values = np.logspace(-3, 3, 3)  # Values for c2
+    hyperparameters = list(product(c1_values, c2_values))  # All combinations of c1 and c2
 
-complete_data = load_data_from_directory(directory)
-train_data, test_data = split_data(complete_data)
-features_train, labels_train = extract_features_and_labels(train_data)
-features_test, labels_test = extract_features_and_labels(test_data)
-hyperparameters, best_performance = hyperparameter_search(features_train, labels_train, hyperparameters, labels_test, features_test)
+    complete_data = load_data_from_file(file)
+    train_data, test_data = split_data(complete_data)
+    print("splitted_data")
+    features_train, labels_train = extract_features_and_labels(train_data)
+    features_test, labels_test = extract_features_and_labels(test_data)
+    hyperparameters, best_performance = hyperparameter_search(features_train, labels_train, hyperparameters, labels_test, features_test, file)
 
-print (hyperparameters, best_performance)
-# Write the best hyperparameters to a file
-with open('hyperparameters.txt', 'w') as f:
-    f.write(f'Best performance: {best_performance}\n')
-    f.write('Best hyperparameters: c1: {c1} c2:{c2}\n')
+    print (hyperparameters, best_performance)
+    # Write the best hyperparameters to a file
+    with open(f'hyperparameters_{filename_base}.txt', 'w') as f:
+        f.write(f'Best performance: {best_performance}\n')
+        f.write('Best hyperparameters: c1: {c1} c2:{c2}\n')
 
-c1, c2 = hyperparameters
-name_classifier_with_best_performance = f'crf_model_c1_{c1}_c2_{c2}.crfsuite'
+    c1, c2 = hyperparameters
+    
+    name_classifier_with_best_performance = os.path.join(output_directory, f'crf_model_{filename_base}_c1_{c1}_c2_{c2}.crfsuite')
 
-# Predict probabilities for test data
 
-probabilities = predict_gene_probability(features_test, name_classifier_with_best_performance)
+    # Plot everything 
+    # Generate true labels and predicted probabilities
+    all_y_pred = []
+    y_true = []
+    for features_test, labels_test in zip(features_test, labels_test):
+        y_pred = predict_gene_probability(
+            features_test, name_classifier_with_best_performance)
+        all_y_pred.extend(y_pred)
+        y_true.extend(labels_test)
+    y_pred = all_y_pred
+    y_true = labels_test
+    y_pred_binary = [["gene" if y_pred_instance >=
+                      0.5 else "no-gene" for y_pred_instance in y_pred]]
+    print(y_pred, y_pred_binary, y_true)
+    # Write the probabilities to a file
+    with open('probabilities_{filename_base}.txt', 'w') as f:
+        f.write(str(y_pred))
 
-# Write the probabilities to a file
-with open('probabilities.txt', 'w') as f:
-    f.write(str(probabilities))
+    plot_confusion_matrix(y_true, y_pred_binary, save_path='confusion_matrix.png')
+    plot_precision_recall_curve(
+        y_true, y_pred, save_path='precision_recall_curve.png')
+    plot_roc_curve(y_true, y_pred, save_path='roc_curve.png')
+    plot_sequence_probabilities(y_true[0], y_pred[0])
 
-# Plot everything 
-# Generate true labels and predicted probabilities
-y_true = labels_test
-y_pred = [[1 if y_pred_instance >= 0.5 else 0 for y_pred_instance in probabilities_per_seq] for probabilities_per_seq in features_test]
+if __name__ == "__main__":
+    # Initialize argument parser
+    parser = argparse.ArgumentParser(
+        description="Train a CRF classifier on genomic data.")
 
-y_true_combined = list(chain(*y_true))
-y_pred_combined = list(chain(*y_pred))
+    # Add arguments
+    parser.add_argument('-f', '--file', required=True,
+                        type=str, help="Input file containing data files")
+    parser.add_argument('-o', '--output_directory', required=True,
+                        type=str, help="Output directory for results")
 
-plot_confusion_matrix(y_true, y_pred, save_path='confusion_matrix.png')
-plot_precision_recall_curve(y_true, probabilities, save_path='precision_recall_curve.png')
-plot_roc_curve(y_true, probabilities, save_path='roc_curve.png')
-plot_sequence_probabilities(y_true[0], probabilities[0])
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Assign input arguments to variables
+    file = args.file
+    print (file)
+    output_directory = args.output_directory
+    main(file, output_directory)
--- Conditional random field classifier/data_preprocession.py
+++ Conditional random field classifier/data_preprocession.py
@@ -20,67 +20,69 @@ def count_kmers(sequence, k):
         d[sequence[i:i+k]] += 1
     return dict(d)
 
-def is_repetitive(db, start, end, sequence_len, strand):
-    if strand == '-':
+def is_repetitive(db, start, end, sequence_len, strand, seq_id):
+    if strand == -1:
         start, end = sequence_len - end, sequence_len - start
     
     # account for 1 based indexing in gff
     start += 1
     end += 1
-    for feature in db.region(region=(start, end)):
-        if feature.featuretype == 'repeat':
+    for feature in db.region(region=(seq_id, start, end)):
+        if 'repeat' in feature.attributes['ID']:
             return 1
     return 0
 
-def is_gene(db, start, end, sequence_len, strand):
-    if strand == '-':
+def is_gene(db, start, end, sequence_len, strand, seq_id):
+    if strand == -1:
         start, end = sequence_len - end, sequence_len - start
     # account for 1 based indexing in gff
     start += 1
     end += 1
-    for feature in db.region(region=(start, end)):
-        if (feature.featuretype == 'gene' or feature.featuretype == 'gene_quality') and feature.strand == strand:
+    for feature in db.region(region=(seq_id, start, end)):
+        #print(strand, feature.strand, feature.featuretype)
+        if (feature.featuretype == 'gene' or feature.featuretype == 'gene_quality') and int(feature.strand) == int(strand):
+            print ("found_gene")
             return 1
     return 0
 
-def process_fasta(fasta_file, gff_file, output_file = None, fragment_size=100, k=3, classification_mode = False):
+def process_fasta(fasta_file, gff_file, output_file = None, fragment_size=1000, k=3, classification_mode = False):
     # Open the GFF database
+    print (gff_file)
     db = gffutils.create_db(gff_file, dbfn='temp.db', force=True,
                             keep_order=True, merge_strategy='merge',
                             sort_attribute_values=True)
 
     fragments = []
-    with open(fasta_file, "r") as handle:
-        for record in SeqIO.parse(handle, "fasta"):
-            sequence = str(record.seq)
-            sequence_rev = str(record.seq.reverse_complement())
-            sequence_len = len(sequence)
+    with open(output_file, 'w') as f:
+        with open(fasta_file, "r") as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                print(record.id)
+                sequence = str(record.seq)
+                sequence_rev = str(record.seq.reverse_complement())
+                sequence_len = len(sequence)
 
-            # Processing both normal and reverse complement strands
-            fragments_in_record = []
-            for seq, strand in [(sequence, '+'), (sequence_rev, '-')]:
-                for i in range(0, sequence_len, fragment_size):
-                    start = i
-                    end = i + fragment_size
-                    fragment = seq[start:end]
-                    features = count_kmers(fragment, k)
-                    features['position'] = start
-                    features['relative_position'] = start/sequence_len
-                    features['strand'] = 1 if strand == "+" else -1
-                    features['repetitive'] = is_repetitive(db, start, end, sequence_len, strand)
-                    if classification_mode == False:
-                        features['gene'] = is_gene(db, start, end, sequence_len, strand)
-                    fragments_in_record.append(features)
+                # Processing both normal and reverse complement strands
+                fragments_in_record = []
+                for seq, strand in [(sequence, +1), (sequence_rev, -1)]:
+                    for i in range(0, sequence_len, fragment_size):
+                        start = i
+                        end = i + fragment_size
+                        fragment = seq[start:end]
+                        features = count_kmers(fragment, k)
+                        features['position'] = start
+                        features['relative_position'] = start/sequence_len
+                        features['strand'] = strand
+                        features['repetitive'] = is_repetitive(db, start, end, sequence_len, strand, record.id)
+                        if classification_mode == False:
+                            features['gene'] = is_gene(db, start, end, sequence_len, strand, record.id)
+                        fragments_in_record.append(features)
                 fragments.append(fragments_in_record)
+                f.write(str(fragments_in_record) + '\n')
 
     # Remove temporary database
     os.remove('temp.db')
 
-    if output_file is not None:
-        # Write fragments to output file
-        with open(output_file, 'w') as f:
-            for fragment in fragments:
-                f.write(str(fragment) + '\n')
+               
     
     return fragments
 
@@ -91,7 +93,7 @@ def main():
     parser.add_argument('output_file', help='The output file to write the fragments to.')
     args = parser.parse_args()
 
-    process_fasta(args.fasta_file, args.gff_file, args.output_file)
+    process_fasta(args.fasta_file, args.gff_file, output_file = args.output_file)
 
 if __name__ == '__main__':
     main()
