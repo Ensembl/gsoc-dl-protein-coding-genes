2913c240867b9433275bd122812b908271ca3d2c
Friederike Biermann <frida@hl-codon-111-01.ebi.ac.uk>
2023-07-12 09:14:31 +0100

    Optimized neural net

--- Conditional random field classifier/crf_classifier_pytorch_one_gpu.py
+++ Conditional random field classifier/crf_classifier_pytorch_one_gpu.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.metrics import classification_report
 import torch.optim as optim
 from torchcrf import CRF
 from torch.nn.utils.rnn import pad_sequence
@@ -31,6 +32,7 @@ class FileIterator:
     def file_generator(self):
         for file_path in self.file_paths:
             with open(file_path, 'r') as file:
+                print (file_path)
                 for line in file:
                     yield line
 
@@ -50,9 +52,12 @@ class GeneDataset(IterableDataset):
                 target_list = []
                 for data in data_list:
                     target = None
-                    fetaures = None
+                    features = None
                     features = {k: parse_number(v) for k, v in data.items() if k != 'gene'}
                     features = add_combinations_with_N(features)
+                    if len(features) != 129:
+                        print("odd number of features in token")
+                        continue
                     target = int(data.get('gene', None))
                     if features and target is not None:
                         features_list.append(torch.tensor([f for f in features.values()]))
@@ -61,15 +66,14 @@ class GeneDataset(IterableDataset):
                     features_list = torch.stack(features_list)
                     target_list = torch.stack(target_list)
                     # Pad to max_sequence_length
-                    print(features_list.shape, target_list.shape)
                     if features_list.size(0) < self.max_sequence_length:
                         pad_size = self.max_sequence_length - features_list.size(0)
-                        padded_features = F.pad(features_list, (0, pad_size), 'constant', 0)
+                        padded_features = F.pad(features_list, (0, 0, pad_size, 0), 'constant', 0)
                     else:
                         padded_features = features_list
                     if target_list.size(0) < self.max_sequence_length:
                         pad_size = self.max_sequence_length - target_list.size(0)
-                        padded_target = F.pad(target_list, (0, pad_size), 'constant', 0)
+                        padded_target = F.pad(target_list, (0, 0, pad_size, 0), 'constant', 0)
                     else:
                         padded_target = target_list
                     yield padded_features, padded_target
@@ -80,7 +84,7 @@ class CRFClassifier(nn.Module):
     def __init__(self, input_dim, num_tags):
         super(CRFClassifier, self).__init__()
         self.feature_extractor = nn.Sequential(
-            nn.Linear(input_dim, 68),
+            nn.Linear(input_dim, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
@@ -90,8 +94,11 @@ class CRFClassifier(nn.Module):
 
     def forward(self, x):
         features = self.feature_extractor(x)
-        output = self.crf.decode(features)
-        return output
+        return features  # Return features
+
+    def decode(self, x):  # Separate method for decoding
+        features = self.feature_extractor(x)
+        return self.crf.decode(features)
 
     def loss(self, x, tags):
         features = self.feature_extractor(x)
@@ -130,23 +137,27 @@ def add_combinations_with_N(features):
     return features
 
 def get_model_predictions_and_labels(model, dataloader):
-    model.eval()
-    all_labels = []
-    all_predictions = []
+    model.eval()  # Set the model to evaluation mode
+    y_true = []
+    y_pred = []
+
     with torch.no_grad():
         for inputs, labels in dataloader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            all_labels.extend(labels.cpu().numpy())
-            all_predictions.extend(outputs)
-    return all_labels, all_predictions
+            outputs = model.decode(inputs)  # Use decode method for inference
+            predicted_labels = outputs  # Assuming the output is a tensor of shape (batch_size, num_classes)
+
+            y_true.extend(labels.tolist())  # Append true labels to y_true list
+            y_pred.extend(predicted_labels)  # Append predicted labels to y_pred list
+    y_true = flatten_list(y_true)
+    y_pred = flatten_list(y_pred)
+    return y_true, y_pred
 
 def train_crf_classifier(train_dataloader, input_dim, num_tags, num_epochs):
     model = CRFClassifier(input_dim, num_tags).to(device)
-
-    optimizer = optim.Adam(model.parameters())
+    learning_rate = 0.001  # Define your learning rate
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     scaler = GradScaler()
-
+    batch_losses = []
     # Create a list to store the loss at each epoch
     epoch_losses = []
     for epoch in range(num_epochs):
@@ -155,17 +166,20 @@ def train_crf_classifier(train_dataloader, input_dim, num_tags, num_epochs):
             inputs, tags = inputs.to(device), tags.to(device)
             optimizer.zero_grad()
             with autocast():
+                tags = tags.squeeze(-1)
                 loss = model.loss(inputs, tags)
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
             total_loss += loss.item()
+            batch_losses.append(loss.item())
+            print(loss.item())
         # Compute average loss for this epoch
-        avg_loss = total_loss / len(train_dataloader)
+        avg_loss = total_loss / (i+1)
         print(f"Epoch {epoch+1}: Loss = {avg_loss}")
         epoch_losses.append(avg_loss)
 
-    return model, epoch_losses  # return losses along with the model
+    return model, epoch_losses, batch_losses  # return losses along with the model
 
 def plot_loss_curve(epoch_losses, save_path=None):
     plt.plot(epoch_losses)
@@ -175,6 +189,24 @@ def plot_loss_curve(epoch_losses, save_path=None):
     if save_path:
         plt.savefig(save_path, dpi=900)
     plt.show()
+
+def plot_batch_losses(batch_losses, save_path=None):
+    plt.plot(batch_losses)
+    plt.title('Training Batch Loss Curve')
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    if save_path:
+        plt.savefig(save_path, dpi=900)
+    plt.show()
+
+def flatten_list(nested_list):
+    flattened_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flattened_list.extend(flatten_list(item))
+        else:
+            flattened_list.append(item)
+    return flattened_list
 
 parser = argparse.ArgumentParser(description='Run the CRF classifier.')
 parser.add_argument('train_data_directory', help='The train_data_directory.')
@@ -191,11 +223,11 @@ train_dataset = GeneDataset(train_directory)
 test_dataset = GeneDataset(test_directory)
 
 # Dataloaders
-train_dataloader = DataLoader(train_dataset, batch_size=32)
-test_dataloader = DataLoader(test_dataset, batch_size=32)
+train_dataloader = DataLoader(train_dataset, batch_size=64)
+test_dataloader = DataLoader(test_dataset, batch_size=64)
 
 # Train the model and get the losses for each epoch
-model, epoch_losses = train_crf_classifier(train_dataloader, input_dim=128, num_tags=2, num_epochs=args.epochs)
+model, epoch_losses, batch_losses = train_crf_classifier(train_dataloader, input_dim=129, num_tags=2, num_epochs=args.epochs)
 
 # Save the model after training
 torch.save(model.state_dict(), os.path.join(output_directory, 'crf.pth'))
@@ -203,8 +235,13 @@ torch.save(model.state_dict(), os.path.join(output_directory, 'crf.pth'))
 # Plot the training loss curve
 plot_loss_curve(epoch_losses, save_path=os.path.join(output_directory, 'training_loss_curve.png'))
 
+# Plot the training batch loss curve
+plot_batch_losses(batch_losses, save_path=os.path.join(output_directory, 'training_batch_loss_curve.png'))
+
 # Assuming you have trained your model and have your dataloader ready:
 y_true, y_pred = get_model_predictions_and_labels(model, test_dataloader)
+
+print(f"Classification report: {classification_report(y_true, y_pred)}")
 
 # Then you can use the data to plot the graphs:
 plot_confusion_matrix(y_true, y_pred, save_path=os.path.join(output_directory, 'confusion_matrix.png'))
